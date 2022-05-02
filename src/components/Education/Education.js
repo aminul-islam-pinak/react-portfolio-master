fa7b0922c31cb1b74760f102873d854695901302
Md.Aminul Islam <md.aminulislampinak@gmail.com>
2022-05-02 16:22:52 +0600

    first commit

---
+++ src/components/Education/Education.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import classes from './Education.module.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+import { MdSchool } from 'react-icons/md';
+import { MdWork } from 'react-icons/md';
+import { FaSchool } from 'react-icons/fa';
+
+class Education extends Component {
+    render() {
+        return (
+            <div className={classes.box} id="education">
+                <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2} animateOnce={true} initiallyVisible={true}>
+                    <span className={classes.head}>MY JOURNEY</span>
+                    <section className={classes.container}>
+                        <div className={classes.container_content}>
+                            <div className={classes.row}>
+                                <div className={classes.row_md_12}>
+                                    <div className={classes.timeline_centered}>
+                                       
+                                        <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+                                            <article className={classes.timeline_entry}>
+                                                <div className={classes.timeline_icon} >
+                                                    <MdSchool />
+                                                </div>
+                                                <div className={classes.label}>
+                                                    <h2 >Undergraduation at IIUC <span>2018-2022</span></h2>
+                                                    <p>I completed my undergraduation in CSE (Computer Science and Engineering) from <a href='https://www.iiuc.ac.bd/'>IIUC</a> with overall 82.71%.</p>
+                                                </div>
+                                            </article>
+                                        </ScrollAnimation>
+                                        <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+                                            <article className={classes.timeline_entry}>
+                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_2}`} >
+                                                    <FaSchool />
+                                                </div>
+                                                <div className={classes.label}>
+                                                    <h2 > Higher Secondary Certificate  <span>2014-2016</span></h2>
+                                                    <p>I have completed my HSC from JHC with major subjects as Physics,Chemistry & Maths with 85% merit in Cumilla board. </p>
+                                                </div>
+                                            </article>
+                                        </ScrollAnimation>
+
+                                        <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+                                            <article>
+                                                <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`} >
+                                                    <MdWork />
+                                                </div>
+                                                <div className={classes.label}>
+                                                    <h2 >Courses</h2>
+                                                    <p>Complete Web Development
+                                                         Course With Jhankar Mahbub.</p>
+                                                         <p> completed 3 months of programming entitled Industrial Traning On PHP & Laravel Full Stack Course.</p>
+                                                </div>
+                                                <div className={classes.timeline_entry_inner}><div className={classes.timeline_icon_3 || classes.color_none}></div></div>
+                                            </article>
+                                        </ScrollAnimation>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </ScrollAnimation>
+            </div>
+
+        )
+    }
+}
+
+export default Education;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+import { HashLink as Link } from 'react-router-hash-link';
+import { FiExternalLink } from 'react-icons/fi';
+import logo from '../images/pinak.jpg'
+
+class Sidebar extends Component {
+    render() {
+
+        return (
+            <div className="sidebar">
+               
+                <h1><Link smooth to="/#start" className="h1_links">Md.Aminul Islam</Link></h1>
+
+                <img src={logo} />
+                <p style={{ color: 'black', fontWeight: 'bold' }} className="gmail"><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJftvBffjPmxWxvfHWRHWnhTbpQgPxBxvkcnPpFpFqbhrXMCHXSJTsCXCVLsQzgRcVLpnPL" rel="opener noreferrer" target="_blank" className="fa fa-envelope"></a> md.aminulislampinak@gmail.com</p>
+
+                <ul className="sidebar-nav">
+                    <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Education</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interest</Link></li>
+                    {/* <li className="sidebar-nav-items"><a href="http://www.bloggingpeek.com" target="_blank" rel="opener noreferrer" className="links"> Blog<FiExternalLink/></a></li> */}
+                </ul>
+
+                <div className="flip-card-back">
+                    <ul className="sidebar-nav">
+                        <li className="sidebar-nav-icons"> <a href="https://github.com/aminul-islam-pinak" rel="opener noreferrer" target="_blank" className="fa fas fa-github fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"><a href="https://www.linkedin.com/in/md-aminul-islam-pinak/" rel="opener noreferrer" target="_blank" className="fa fas fa-linkedin fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://www.instagram.com/aminul_islam_pinak/?hl=en" rel="opener noreferrer" target="_blank" className="fa fas fa-instagram fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="md.aminulislampinak@gmail.com" rel="opener noreferrer" target="_blank" className="fa fas fa-envelope fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://www.facebook.com/muhammadaminulislam.pinak.9/" rel="opener noreferrer" target="_blank" className="fa fas fa-facebook fa-lg"></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Sidebar
